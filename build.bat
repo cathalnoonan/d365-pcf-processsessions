f3f8d91d145dc602335a5d006f69509605c091cc
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-12-15 21:13:41 +0000

    Build updates

--- build.bat
+++ build.bat
@@ -1,11 +1,8 @@
 @echo off
 
-CALL yarn --cwd ./control
+cd ./control
+call npm install --no-audit
+call npm audit --prod
+cd ..
 
-SET msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
-
-if exist ./solution/dist/processsessions.zip (
-    %msbuild% ./solution/solution.cdsproj /t:build /p:Configuration=Release
-) else (
-    %msbuild% ./solution/solution.cdsproj /t:build /restore /p:Configuration=Release
-)
+call dotnet build ./solution -c Release
--- clean.bat
+++ clean.bat
@@ -1,10 +1,3 @@
 @echo off
 
-CALL npx rimraf ./control/node_modules
-CALL npx rimraf ./control/obj
-CALL npx rimraf ./control/out
-CALL npx rimraf ./control/ProcessSessions/out
-
-CALL npx rimraf ./solution/bin
-CALL npx rimraf ./solution/obj
-CALL npx rimraf ./solution/dist
+call npx rimraf ./**/node_modules ./**/bin ./**/obj ./**/out ./**/dist
